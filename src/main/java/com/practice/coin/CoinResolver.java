149c3778bc980b9e76886ccd211f60d2fbba6767
anpasi <antonio_palomares@yahoo.es>
2021-08-20 21:49:45 +0100

    Add test case to complete class functionality

--- src/main/java/com/practice/coin/CoinResolver.java
+++ src/main/java/com/practice/coin/CoinResolver.java
@@ -1,5 +1,28 @@
 package com.practice.coin;
 
 public class CoinResolver {
+	
+	
+	/**
+	 * Converts an amount in pounds into a into a string that represents its composition in coins from greatest to lowest
+	 * @param the string that represents the amount. The format of the amount is
+	 *    <ul>
+	 *    	<li>An number in integer format (e.g. 200)</li>
+	 *    	<li>An number in double format (e.g. 200.00)</li>
+	 *    	<li>The pound symbol and an amount in pounds in either integer or double format (e.g.: £20, £20.01) </li>
+	 *    </ul>
+	 * @return a string that represents the input amount in coins
+	 * @throws Exception 
+	 */
+	public String getCoinsInfo(String input) {
+		
+		if (input == null || input.length()==0) {
+			throw new RuntimeException("Wrong length");
+		}
+		
+		
+		
+		return null;
+	}
 
 }
--- src/test/java/com/practice/coin/CoinResolverTest.java
+++ src/test/java/com/practice/coin/CoinResolverTest.java
@@ -1,10 +1,16 @@
 package com.practice.coin;
 
+import org.junit.Test;
+
 public class CoinResolverTest {
 	
 	
 	private CoinResolver coinResolver = new CoinResolver();
 	
-	
+	@Test(expected = RuntimeException.class)
+	public void testInvalidInputLength() {
+		String input = "";
+		String result = coinResolver.getCoinsInfo(input);
+	}
 
 }
